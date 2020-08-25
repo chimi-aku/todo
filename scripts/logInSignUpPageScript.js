23b3dcacd2fe40c65d4b649daa618d99bc740b39
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2020-08-25 13:26:16 +0200

    restart project

--- scripts/logInSignUpPageScript.js
+++
@@ -1,4 +0,0 @@
-const signUpUsernameInput = document.querySelector('.register_box .input_username');
-const signUpPasswordInput = document.querySelector('.register_box .input_password');
-const signUpBtn = document.querySelector
-console.log(signUpUsernameInput);
