037d4cc3e2f6d18bf90a080d349805f60a8a8784
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2020-08-25 12:59:19 +0200

    create simple design

---
+++ scripts/logInSignUpPageScript.js
@@ -0,0 +1,4 @@
+const signUpUsernameInput = document.querySelector('.register_box .input_username');
+const signUpPasswordInput = document.querySelector('.register_box .input_password');
+const signUpBtn = document.querySelector
+console.log(signUpUsernameInput);
